a65d8c9a48dc047c6a0b8818f6d6bf929ad92012
mvaib <mvaibhav596@gmail.com>
2025-03-29 15:20:10 +0530

    updated

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const PORT = 8080
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin : "http://localhost:5173/",
+    methods : ["GET","POST","PUT","DELETE"],
+    credentials : true
+}))
 
 // api endpoint
 
